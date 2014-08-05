85a3fc686985777faf0b3c0f9dc38c583634b930
Charles J. Ezell III <charles.ezell@gmail.com>
2014-08-04 23:59:09 -0400

    Added callbacks for rest of the normal test cases.

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,12 @@ var aggregate = require("mongodb-pipeline").aggregate,
 	assert = require("assert"),
 	sampleDocs1 = [{"v":3}, {"v":5}, {"v":2}, {"v":1}, {"v":4}];
 
+function compareResulDocs(expected, done, err, actual){
+	if(err) return done(err)
+	assert.deepEqual(actual, expected);
+	return done();
+}
+
 describe("aggregate", function(){
 	describe("error handling", function(){
 		it("should require three arguments", function(done){
@@ -42,52 +48,50 @@ describe("aggregate", function(){
 		});
 	});
 
-	describe("$limit", function(){
-		it("should be able to limit the actual set", function(){
-			var actual = aggregate([{$limit:1}], sampleDocs1);
-			assert.deepEqual(actual, [{"v":3}]);
+	describe("document sources", function(){
+		describe("$limit", function(){
+			it("should be able to limit the actual set", function(done){
+				var expected = [{"v":3}];
+				aggregate([{$limit:1}], sampleDocs1, compareResulDocs.bind(null, expected, done));
+			});
 		});
-	});
 
-	describe("$match", function(){
-		it("should be able to do a simple match", function(){
-			var expected = [{"v":3},{"v":5},{"v":4}],
-				actual = aggregate([{$match:{"v":{"$gte":3, "$lt":6}}}], sampleDocs1);
-			assert.deepEqual(actual, expected);
+		describe("$match", function(){
+			it("should be able to do a simple match", function(done){
+				var expected = [{"v":3},{"v":5},{"v":4}];
+				aggregate([{$match:{"v":{"$gte":3, "$lt":6}}}], sampleDocs1, compareResulDocs.bind(null, expected, done));
+			});
 		});
-	});
 
-	describe("$project", function(){
-		it("should be able to rename a field", function(){
-			var expected = [{"bar":3},{"bar":5},{"bar":2},{"bar":1},{"bar":4}],
-				actual = aggregate([{$project:{"bar":"$v"}}], sampleDocs1);
-			assert.deepEqual(actual, expected);
+		describe("$project", function(){
+			it("should be able to rename a field", function(done){
+				var expected = [{"bar":3},{"bar":5},{"bar":2},{"bar":1},{"bar":4}];
+				aggregate([{$project:{"bar":"$v"}}], sampleDocs1, compareResulDocs.bind(null, expected, done));
+			});
 		});
-	});
 
-	describe("$sort", function(){
-		it("should be able to sort a simple set of documents", function(){
-			var expected = [{"v":5},{"v":4},{"v":3},{"v":2},{"v":1}],
-				actual = aggregate([{$sort:{"v":-1}}], sampleDocs1);
-			assert.deepEqual(actual, expected);
+		describe("$sort", function(){
+			it("should be able to sort a simple set of documents", function(done){
+				var expected = [{"v":5},{"v":4},{"v":3},{"v":2},{"v":1}];
+				aggregate([{$sort:{"v":-1}}], sampleDocs1, compareResulDocs.bind(null, expected, done));
+			});
 		});
-	});
 
-	describe("$group", function(){
-		it("should be able to group on null", function(){
-			var expected = [{"_id":null,"sum":15,"count":5}],
-				actual = aggregate([{
+		describe("$group", function(){
+			it("should be able to group on null", function(done){
+				var expected = [{"_id":null,"sum":15,"count":5}];
+				aggregate([{
 						$group:{
 							_id:null,
 							sum:{$sum:"$v"},
 							count:{$sum:1}
 						}}],
-						sampleDocs1
+						sampleDocs1,
+						compareResulDocs.bind(null, expected, done)
 					);
-			assert.deepEqual(actual, expected);
+			});
 		});
 	});
-
 });
 
 describe("_aggregate_bare", function(){
